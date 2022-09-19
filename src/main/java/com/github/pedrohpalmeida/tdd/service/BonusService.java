7b5e660a6419bffa1a0ffa3b8a9c041d034a59b6
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-09-19 10:34:45 -0300

    Projeto Inicial

---
+++ src/main/java/com/github/pedrohpalmeida/tdd/service/BonusService.java
@@ -0,0 +1,17 @@
+package com.github.pedrohpalmeida.tdd.service;
+
+import java.math.BigDecimal;
+
+import com.github.pedrohpalmeida.tdd.modelo.Funcionario;
+
+public class BonusService {
+
+	public BigDecimal calcularBonus(Funcionario funcionario) {
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		if (valor.compareTo(new BigDecimal("1000")) > 0) {
+			valor = BigDecimal.ZERO;
+		}
+		return valor;
+	}
+
+}
