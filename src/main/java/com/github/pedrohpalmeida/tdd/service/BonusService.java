44443ef03311d6d18bd0cd27402015f4dcabd733
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-09-19 15:41:52 -0300

    refact: refatorando método para lançar Exception

--- src/main/java/com/github/pedrohpalmeida/tdd/service/BonusService.java
+++ src/main/java/com/github/pedrohpalmeida/tdd/service/BonusService.java
@@ -10,7 +10,7 @@ public class BonusService {
 	public BigDecimal calcularBonus(Funcionario funcionario) {
 		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;
+			throw new IllegalArgumentException("Funcionário com salário maior do que R$ 10000,00 não pode receber bônus");
 		}
 		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
